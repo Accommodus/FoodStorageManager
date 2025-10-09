1928d78894d5e18d30dc0dda5ddc8e523196a51b
Cannon Whitney <cannon.whitney@ufl.edu>
2025-10-09 23:40:53 +0000

    Ensure server can connect to db

--- server/src/server.ts
+++ server/src/server.ts
@@ -11,17 +11,17 @@ app.use(express.urlencoded({ extended: true }));
 const mongoUri: string = process.env.MONGODB_URI ?? (() => { throw new Error('Missing MONGODB_URI'); })();
 const port: string = process.env.S_PORT ?? (() => { throw new Error('Missing Server Port'); })();
 
-(async () => {
+app.get('/health', async (_req: express.Request, res: express.Response) => {
+    let health: string;
+
     try {
         await mongoose.connect(mongoUri);
-        console.log('Connected to the database');
-    } catch(error) {
-        console.error(error);
+        health = 'Connected to the database';
+    } catch(e) {
+        health = e instanceof Error ? e.message : String(e);
     }
-})();
 
-app.get('/health', (_req: express.Request, res: express.Response) => {
-    res.status(200).send('Server is running');
+    res.status(200).send(health);
 });
 
 app.listen(port, () => {
