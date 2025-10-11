fcc1a9f015b78238b6e4671216a23240b94b252e
ellikon <robbie@millertribe.com>
2025-10-11 02:09:07 -0400

    Added base font and colors

--- client/src/pages/Inventory.tsx
+++ client/src/pages/Inventory.tsx
@@ -1,5 +1,7 @@
 const Inventory = () => {
-    return <h1>Inventory</h1>;
+    return (
+        <h1 className="bg-green text-5xl font-bold tracking-wide">Inventory</h1>
+    );
 };
 
 export default Inventory;
