c2dfe3afdee442ac56b6dc93dc287d012e50b76a
ellikon <robbie@millertribe.com>
2025-10-11 02:26:17 -0400

    Added file structure for inventory list and items

---
+++ client/src/features/InventoryList/InventoryList.tsx
@@ -0,0 +1,23 @@
+import InventoryItem from './InventoryItem';
+
+export const InventoryList = () => {
+    return (
+        <ul>
+            <li>
+                <InventoryItem />
+            </li>
+            <li>
+                <InventoryItem />
+            </li>
+            <li>
+                <InventoryItem />
+            </li>
+            <li>
+                <InventoryItem />
+            </li>
+            <li>
+                <InventoryItem />
+            </li>
+        </ul>
+    );
+};
--- client/src/pages/Inventory.tsx
+++ client/src/pages/Inventory.tsx
@@ -1,6 +1,13 @@
+import { InventoryList } from '../features/InventoryList';
+
 const Inventory = () => {
     return (
-        <h1 className="bg-green text-5xl font-bold tracking-wide">Inventory</h1>
+        <>
+            <h1 className="bg-green text-5xl font-bold tracking-wide">
+                Inventory
+            </h1>
+            <InventoryList />
+        </>
     );
 };
 
