1002c9099321ba1b67da7f1391b93fc9ac14c2ba
Cannon Whitney <cannon.whitney@ufl.edu>
2025-10-12 20:13:02 +0000

    Optimize server by maintaining a single persistent database connection

--- server/src/db.ts
+++ server/src/db.ts
@@ -1,13 +1,17 @@
-import mongoose from 'mongoose';
-import {MaybeError} from './types'
+import mongoose from "mongoose";
+import { ServerHealth } from "./types";
 
-const mongoUri: string = process.env.MONGODB_URI ?? (() => { throw new Error('Missing MONGODB_URI'); })();
+const mongoUri: string =
+  process.env.MONGODB_URI ??
+  (() => {
+    throw new Error("Missing MONGODB_URI");
+  })();
 //const mongoUri: string = process.env.MONGODB_URI ?? "mongodb://db:27017/local"
 
-export async function connectDB(uri: string = mongoUri): Promise<MaybeError> {
+export function connectDB(uri: string = mongoUri): ServerHealth {
   try {
-    await mongoose.connect(uri);
-    return { ok: true };
+    mongoose.connect(uri);
+    return { ok: true, value: mongoose.connection };
   } catch (err) {
     const error = err instanceof Error ? err : new Error(String(err));
     return { ok: false, error };
