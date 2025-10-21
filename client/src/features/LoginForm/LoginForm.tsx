030b351d90608b51389421c9d80145502c84f5c9
ellikon <robbie@millertribe.com>
2025-10-21 23:08:08 +0000

    Added basic login form logic

--- client/src/features/LoginForm/LoginForm.tsx
+++ client/src/features/LoginForm/LoginForm.tsx
@@ -1,14 +1,35 @@
+import { useForm } from 'react-hook-form';
+import type { SubmitHandler } from 'react-hook-form';
 import { BiSolidEnvelope, BiSolidLockAlt } from 'react-icons/bi';
 
-export const LoginForm = () => {
+type LoginFormProps = {
+    submitHandler: (data: FormInputs) => void;
+};
+
+type FormInputs = {
+    email: string;
+    password: string;
+};
+
+export const LoginForm = ({ submitHandler }: LoginFormProps) => {
+    const { register, handleSubmit } = useForm<FormInputs>();
+
+    const onSubmit: SubmitHandler<FormInputs> = (data) => {
+        submitHandler(data);
+    };
+
     return (
-        <form className="m-auto flex w-100 flex-col gap-4 rounded-2xl bg-neutral-100 px-16 py-8 shadow-lg">
+        <form
+            className="m-auto flex w-100 flex-col gap-4 rounded-2xl bg-neutral-100 px-16 py-8 shadow-lg"
+            onSubmit={handleSubmit(onSubmit)}
+        >
             <div className="relative flex gap-2">
                 <BiSolidEnvelope className="absolute left-2 size-6 self-center text-neutral-400" />
                 <input
                     className="bg-neutral-150 w-full rounded-lg py-2 pr-4 pl-10 text-lg text-neutral-900"
                     type="email"
                     placeholder="Email"
+                    {...register('email')}
                 />
             </div>
             <div className="relative flex gap-2">
@@ -17,6 +38,7 @@ export const LoginForm = () => {
                     className="bg-neutral-150 w-full rounded-lg py-2 pr-4 pl-10 text-lg text-neutral-900"
                     type="password"
                     placeholder="Password"
+                    {...register('password')}
                 />
             </div>
             <button
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,9 +1,22 @@
 import { LoginForm } from '@features/LoginForm';
+import { useState, useEffect } from 'react';
 
 const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        console.log('Email: ' + email + ', Password: ' + password);
+    }, [email, password]);
+
+    const handleSubmit = (data: { email: string; password: string }) => {
+        setEmail(data.email);
+        setPassword(data.password);
+    };
+
     return (
         <div className="mt-16">
-            <LoginForm />
+            <LoginForm submitHandler={handleSubmit} />
         </div>
     );
 };
