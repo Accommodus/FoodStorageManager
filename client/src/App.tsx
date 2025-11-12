1eb986bd36d9196f90b66278d77a3208432c742d
ellikon <robbie@millertribe.com>
2025-11-12 19:18:43 +0000

    Added basic routing with react router

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,23 @@
-//import Login from './pages/Login.tsx';
+import Login from './pages/Login.tsx';
+import AppLayout from './pages/AppLayout.tsx';
 import Inventory from './pages/Inventory.tsx';
-import { NavBar } from '@features/ui/NavBar';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router';
 
 const App = () => {
     return (
-        <>
-            <NavBar />
-            <Inventory />
-        </>
+        <BrowserRouter>
+            <Routes>
+                <Route index element={<Navigate to="login" replace />} />
+                <Route path="login" element={<Login />} />
+                <Route path="app" element={<AppLayout />}>
+                    <Route
+                        index
+                        element={<Navigate to="inventory" replace />}
+                    />
+                    <Route path="inventory" element={<Inventory />} />
+                </Route>
+            </Routes>
+        </BrowserRouter>
     );
 };
 
--- client/src/features/ui/NavBar/NavBar.tsx
+++ client/src/features/ui/NavBar/NavBar.tsx
@@ -1,6 +1,6 @@
 export const NavBar = () => {
     return (
-        <div className="h-8 w-full bg-neutral-900">
+        <div className="flex h-16 w-full items-center justify-around bg-neutral-900 text-neutral-50">
             <p>Navbar</p>
         </div>
     );
