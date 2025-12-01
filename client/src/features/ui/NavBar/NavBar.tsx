6141eab96b353b2b63b3542d7626ec868000cf0a
Cannon Whitney <cannon.whitney@ufl.edu>
2025-12-01 01:49:13 +0000

    Remove account information from NavBar

--- client/src/features/ui/NavBar/NavBar.tsx
+++ client/src/features/ui/NavBar/NavBar.tsx
@@ -5,7 +5,6 @@ export const NavBar = () => {
         <div className="flex h-16 w-full items-center bg-neutral-900 text-neutral-50">
             <nav className="flex flex-1 items-center justify-around">
                 <NavLink to={'/app/inventory'}>Inventory</NavLink>
-                <NavLink to={'/app/'}>Account Information</NavLink>
                 <NavLink to={'/app/users'}>Users</NavLink>
             </nav>
         </div>
